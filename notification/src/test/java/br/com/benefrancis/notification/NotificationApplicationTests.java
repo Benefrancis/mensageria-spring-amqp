81a6c1c025a0c465ad99a52d1b1567225c9a17f9
Benefrancis do Nascimento <benefrancis@gmail.com>
2022-07-07 13:32:17 -0300

    criando projetos

---
+++ notification/src/test/java/br/com/benefrancis/notification/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.benefrancis.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
