61c91146c0b0875f4627a5160d771e19a7bdf04c
Benefrancis do Nascimento <benefrancis@gmail.com>
2022-07-07 22:11:13 -0300

    Update OrderController.java

--- order/src/main/java/br/com/benefrancis/order/controller/OrderController.java
+++ order/src/main/java/br/com/benefrancis/order/controller/OrderController.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.benefrancis.order.entity.Order;
 import br.com.benefrancis.order.repository.OrderRepository;
@@ -27,13 +29,21 @@ public class OrderController {
 	private RabbitTemplate rabbitTemplate;
 
 	@PostMapping
-	public Order create(@RequestBody Order order) {
+	public ResponseEntity<Order> create(@RequestBody Order order) {
 		orders.save(order);
 		OrderCreatedEvent event = new OrderCreatedEvent(order.getId(), order.getValue());
 		String exchange = "orders.v1.order-created";
 		String routingKey = "";
 		rabbitTemplate.convertAndSend(exchange, routingKey, event);
-		return order;
+
+		// @formatter:off
+ 		return ResponseEntity.created(
+				ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(order.getId()).toUri())
+ 				.body(order);
+ 		// @formatter:on
+
 	}
 
 	@GetMapping
@@ -42,8 +52,17 @@ public class OrderController {
 	}
 
 	@GetMapping("{id}")
-	public Order findById(@PathVariable Long id) {
-		return orders.findById(id).orElseThrow();
+	public ResponseEntity<Order> findById(@PathVariable Long id) {
+
+		Order resp = orders.findById(id).orElse(null);
+
+		if (resp == null) {
+			return ResponseEntity.notFound().build();
+
+		} else {
+			return ResponseEntity.ok(resp);
+		}
+
 	}
 
 	@PutMapping("{id}/pay")
